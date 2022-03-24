2f1aa4d2c59df6b64f8e90b6b078fa825e5e6d54
Alberto Mori <albi.mo87@hotmail.it>
2022-03-24 18:47:05 +0100

    enable authentication

--- src/BlazorSwaDemo.Client/Program.cs
+++ src/BlazorSwaDemo.Client/Program.cs
@@ -1,10 +1,13 @@
+using AzureStaticWebApps.Blazor.Authentication;
+using BlazorSwaDemo.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorSwaDemo.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddStaticWebAppsAuthentication();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
